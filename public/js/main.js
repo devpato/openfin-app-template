55be7106a5f14949c5767ba5a4d4deaa2b2835c8
Pato <pevargas2730@gmail.com>
2020-02-17 13:34:37 -0500

    playing with platform api

--- public/js/main.js
+++ public/js/main.js
@@ -1,34 +1,41 @@
-// uncomment line below to register offline cache service worker 
-// navigator.serviceWorker.register('../serviceworker.js');
+// uncomment line below to register offline cache service worker
+navigator.serviceWorker.register("../serviceworker.js");
 
-if (typeof fin !== 'undefined') {
+if (typeof fin !== "undefined") {
     init();
 } else {
-    document.querySelector('#of-version').innerText =
-        'The fin API is not available - you are probably running in a browser.';
+    document.querySelector("#of-version").innerText =
+        "The fin API is not available - you are probably running in a browser.";
 }
 
 //once the DOM has loaded and the OpenFin API is ready
 async function init() {
-    //get a reference to the current Application.
-    const app = await fin.Application.getCurrent();
-    const win = await fin.Window.getCurrent();
+    let platform = await fin.Platform.getCurrent();
+    let myWinIdentity = await platform.createWindow({
+        contextMenu: true,
+        layout: {
+            content: [
+                {
+                    type: "stack"
+                }
+            ]
+        }
+    });
 
-    const ofVersion = document.querySelector('#of-version');
-    ofVersion.innerText = await fin.System.getVersion();
+    // The `platform`and `myWinIdentity` here are the objects we captured in step 2
+    platform.createView(
+        {
+            // View Configuration Options
+            name: "my-new-test-view-2",
+            url: "http://www.firebase.com"
+        },
+        myWinIdentity // Target Identity
+    );
 
-    //Only launch new windows from the main window.
-    if (win.identity.name === app.identity.uuid) {
-        //subscribing to the run-requested events will allow us to react to secondary launches, clicking on the icon once the Application is running for example.
-        //for this app we will  launch a child window the first the user clicks on the desktop.
-        app.once('run-requested', async () => {
-            await fin.Window.create({
-                name: 'childWindow',
-                url: location.href,
-                defaultWidth: 320,
-                defaultHeight: 320,
-                autoShow: true
-            });
-        });
-    }
+    // The `platform` here is the object we captured in step 2
+    const mySnapshot = await platform.getSnapshot();
+    console.log(mySnapshot);
+
+    // The `platform` here is the object we captured in step 2
+    // platform.applySnapshot(mySnapshot, { closeExistingWindows: true });
 }
